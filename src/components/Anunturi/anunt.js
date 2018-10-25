a47e2d42facaabfe26de92f4d74149bbc79b9746
DRT <daniriian@gmail.com>
2018-10-25 14:58:54 +0300

    anunt in progress

--- src/components/Anunturi/anunt.js
+++ src/components/Anunturi/anunt.js
@@ -2,35 +2,21 @@ import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import "./anunt.css";
-import FileIcon, { defaultStyles } from "react-file-icon";
+// import FileIcon, { defaultStyles } from "react-file-icon";
 
 class Anunt extends Component {
   render() {
     return (
       <div className="anunt">
-        <Grid container spacing={16} justify="flex-end">
-          <Grid item xs={2} style={{ textAlign: "right" }}>
-            <FileIcon extension="pdf" {...defaultStyles.pdf} size={40} />
-          </Grid>
-          <Grid item xs={10} sm={8}>
-            <div className="inline">
-              <div className="inline">
-                <p>
-                  Anunt fdqg gfd gfd gfdgfsdfdsa grfdsgfd ggdfs gfdsgdf fwewqf
-                  few fewqfwqe fweqew fewqf fewq fwqe fwq fewq qewf few qew
-                  fqwfe rewqrwrw rewqrewrq rewq rewr reqw rew qwer rew rewq qewr
-                  r rew q qewrr ew r rewqrewq erwfdeqwfc cwqecewqefcw
-                  cewqcewqceqwcewq cewcewqew cewqcewq cewqceqw ewcw q
-                </p>
-              </div>
+        <Grid container>
+          <Grid item xs={12} sm={9}>
+            <div className="docIcon inline">icon</div>
+            <div className="textContent inline">
+              <p>Cereroferta rechizite - Tribunalul Cluj</p>
             </div>
           </Grid>
-          <Grid
-            item
-            sm={2}
-            style={{ textAlign: "center", backgroundColor: "red" }}
-          >
-            Deschide
+          <Grid item xs={12} sm={3} style={{ textAlign: "center", border: "1px solid red" }} >
+            <button>Deschide</button>
           </Grid>
         </Grid>
       </div>
