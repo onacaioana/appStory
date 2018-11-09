9d20a6c4fea28ed16ed9e48ab78d7a391ae56796
onacaioana <ioanaonaca92@gmail.com>
2018-11-09 12:36:32 +0200

    routes update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from "react";
 import "./App.css";
-import { HashRouter , Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import Contact from "./components/contact";
 import Prezentare from "./components/prezentare";
 import HomePage from "./components/home";
@@ -15,14 +15,15 @@ import Concursuri from './components/concursuri';
 import Declaratii from './components/declaratii';
 import InfoPublice from "./components/infoPublice";
 import Comunicate from "./components/comunicate";
+import ErrorPage1 from './components/errorPage';
 const { detect } = require('detect-browser');
 const browser = detect();
 
 
 class App extends Component {
   render() {
-    console.log(browser.version.substr(0,2).toString);
-    if (browser.name === 'ie' && browser.version.substr(0,2) ==='9') {
+    console.log(browser.version.substr(0, 2).toString);
+    if (browser.name === 'ie' && browser.version.substr(0, 2) === '9') {
       return this.renderForNoCompatibility();
     }
     else return this.renderAll();
@@ -30,34 +31,44 @@ class App extends Component {
   renderForNoCompatibility() {
     return (
       <div>
-        <p>Acest site nu suporta aceasta versiune de browser. Va rugam sa updataţi browserul dumneavostră sau să folositi Chorme sau FireFox </p>
+        <p>Acest site nu suporta aceasta versiune de browser. Va rugam sa updataţi browserul dumneavostră sau să folositi Chrome sau FireFox </p>
       </div>
     )
   }
 
   renderAll() {
     const baseUrl = process.env.PUBLIC_URL;
-    console.log({baseUrl});
+    console.log({ baseUrl });
     return (
       <React.Fragment>
 
-        <Meniu />
         <HashRouter basename={baseUrl} component={App}>
-          <Switch>
-            <Route exact path={"/organizare"} component={Organizare} />
-            <Route exact path={"/contact"} component={Contact} />
-            <Route exact path={"/taxe"} component={TaxeTimbru} />
-            <Route exact path={"/prezentare"} component={Prezentare} />
-            <Route exact path={"/rapoarte"} component={Rapoarte} />
-            <Route exact path={"/hotarari"} component={Hotarari} />
-            <Route exact path={"/concurs"} component={Concursuri} />
-            <Route exact path={"/declaratii"} component={Declaratii} />
-            <Route exact path={"/infoPublice"} component={InfoPublice} />
-            <Route exact path={"/comunicate"} component={Comunicate} />
-            <Route exact path={"/"} component={HomePage} />
-          </Switch>
+          <div>
+            <Switch>
+              <Route exact path={['/organizare', '/contact', '/taxe', '/prezentare', '/rapoarte', '/hotarari', '/concurs', '/declaratii', '/infoPublice', '/comunicate', '/']} component={Meniu} />
+            </Switch>
+
+            <Switch>
+              <Route exact path={"/organizare"} component={Organizare} />
+              <Route exact path={"/contact"} component={Contact} />
+              <Route exact path={"/taxe"} component={TaxeTimbru} />
+              <Route exact path={"/prezentare"} component={Prezentare} />
+              <Route exact path={"/rapoarte"} component={Rapoarte} />
+              <Route exact path={"/hotarari"} component={Hotarari} />
+              <Route exact path={"/concurs"} component={Concursuri} />
+              <Route exact path={"/declaratii"} component={Declaratii} />
+              <Route exact path={"/infoPublice"} component={InfoPublice} />
+              <Route exact path={"/comunicate"} component={Comunicate} />
+              <Route exact path={"/"} component={HomePage} />
+              <Route path="*" component={ErrorPage1} status={404} />
+
+            </Switch>
+
+            <Switch>
+              <Route exact path={['/organizare', '/contact', '/taxe', '/prezentare', '/rapoarte', '/hotarari', '/concurs', '/declaratii', '/infoPublice', '/comunicate', '/']} component={Footer} />
+            </Switch>
+          </div>
         </HashRouter >
-        <Footer />
       </React.Fragment>
     );
   }
---
+++ src/components/errorPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+const ErrorPage1 = () => (
+    <Typography align="center">
+        Eroare ! Pagina inexistenta !
+    </Typography>
+)
+
+export default ErrorPage1;
