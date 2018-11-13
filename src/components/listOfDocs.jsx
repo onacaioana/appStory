fe5820f907ea67137daf118bae0641d0eca00788
onacaioana <ioanaonaca92@gmail.com>
2018-11-13 08:19:47 +0200

    conflict solved

--- src/components/listOfDocs.jsx
+++ src/components/listOfDocs.jsx
@@ -7,48 +7,6 @@ import Pdf from './Pdf/pdf';
 import axios from 'axios';
 
 class ListOfDocs extends Component {
-<<<<<<< HEAD
-    constructor(props){
-        super(props);
-    };
-    state={
-        blob:[],
-        openFile:12
-    };
-    /* Const ITEMS create each ListItem from ListOfDocs */
-    handleClick = (fileName,e,index) => {
-        console.log(e);
-        axios.get(`/ass`,{ params: {
-            fileName: fileName
-          }})
-            .then(res => {
-                this.setState({
-                    blob: res.data
-                });
-                this.setState({openFile:index})
-            })
-    }
-
-    render() {
-        const items = this.props.list.map((item, index) => {
-            console.log(item,index);
-            return (
-                <ListItem key={index} className="mx-5 px-5 " button={this.props.button} onClick={(e) => this.handleClick(item.locatie,e,index)}  >
-                    {this.state.openFile == index ?  <Pdf data={`data:application/pdf;base64,${this.state.blob}`} print={this.state.blob} fileName={item.locatie}></Pdf> : ""}
-                    {this.props.icon
-                        ? <ListItemIcon className="mx-1 px-1">
-                            <img src={this.props.icon} alt="Icon_Lista_De_Documente"></img>
-                        </ListItemIcon>
-                        : ""}
-                    <ListItemText color="white" inset primary={item.titlu} secondary={item.data} />
-                </ListItem>
-
-            )
-        });
-        return (
-            <List component={this.props.component} disablePadding subheader={this.props.subheader}>
-                {items}
-=======
     constructor(props) {
         super(props);
         this.state = {
@@ -93,7 +51,6 @@ class ListOfDocs extends Component {
         return (
             <List component={this.props.component} disablePadding subheader={this.props.subheader} >
                 {this.state.items}
->>>>>>> pr/3
             </List>
         )
     }
