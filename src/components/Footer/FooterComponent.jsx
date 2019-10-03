6937657dc2c5f27f83bed4a0d076d4a3aa03317f
onacaioana <ioanaonaca92@gmail.com>
2019-10-03 09:38:14 +0300

    Update FooterComponent.jsx styling
    FooterComponent.jsx  was restyled using styled-compoments

--- src/components/Footer/FooterComponent.jsx
+++ src/components/Footer/FooterComponent.jsx
@@ -1,21 +1,45 @@
 import React from 'react';
-import './Footer.css';
+import styled from 'styled-components';
+
+const FooterContainer = styled.div`
+    border-top: 1px dashed rgba(255, 255, 255, 0.1);
+    background-color: #1f1f1f;
+    box-sizing: border-box;
+    width: 100%;
+    text-align: center;
+    padding: 30px 0px;
+    overflow: hidden;
+`
+
+const FooterDistributed = styled.div`
+    .footer-company-name{
+        font-weight: 300;
+        margin: 0 10px;
+        color: #666;
+        padding: 0;
+    }
+    .a{
+        margin-bottom: 20px;
+        display: inline-block;
+    }
+    .image{
+        width: 5em;
+    }
+`
 
 const FooterPage1 = () => {
     return (
-        <div className="copyrights">
-            <div className="container">
-                <div className="footer-distributed">
-                    <a href="/home"> <img style={{ width: '20%' }} src={require("../../images/ministerul_justitiei.PNG")} alt="logo-ministerul-justitiei" /></a>
-                    <div className="footer-center">
-                        <p className="footer-company-name">
-                            All Rights Reserved. &copy; 2019. Design By :
-					                <a href="http://www.tribunalulcluj.ro/">Compartimentul IT - Tribunalul Cluj</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <FooterContainer>
+                <FooterDistributed>
+                    <a href="/home"> 
+                        <img className="image" src={require("../../images/ministerul_justitiei.PNG")} alt="logo-ministerul-justitiei" />
+                    </a>
+                    <p className="footer-company-name">
+                        All Rights Reserved. &copy; 2019. Design By :
+                        <a href="http://www.tribunalulcluj.ro/">Compartimentul IT - Tribunalul Cluj</a>
+                    </p>      
+                </FooterDistributed>
+        </FooterContainer>
     );
 }
 
